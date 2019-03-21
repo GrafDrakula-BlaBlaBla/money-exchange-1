86d32f48ec62843cd636281fc4f5cd51b9896587
GrafDracula_BlaBlaBla <leo95_11@mail.ru>
2019-03-22 01:00:58 +0300

    Task complete

--- src/index.js
+++ src/index.js
@@ -1,5 +1,30 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    if(currency > 10000){
+        return {error: "You are rich, my friend! We don't have so much coins for exchange"};
+    }
+    
+    let cost = {
+        H : 50,
+        Q : 25,
+        D : 10,
+        N : 5,
+        P : 1
+    };
+
+    let result = {};
+
+    if(currency <= 0){
+        return {};
+    }else {
+        for(let key in cost){
+            if(cost.hasOwnProperty(key)){
+                while(cost[key] <= currency) {
+                    result[key] = Math.floor(currency/cost[key]);
+                    currency -= (cost[key] * result[key]);
+                }
+            }
+        }   
+    }
+    return result;
 }
